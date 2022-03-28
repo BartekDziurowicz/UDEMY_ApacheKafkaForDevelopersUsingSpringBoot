32a131f2c42e2d36da4f4a9821c0a042aed74286
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-03-28 08:54:16 +0200

    8.33 Sending KafkaRecord With Headers using KafkaTemplate

--- src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -50,7 +53,7 @@ public class LibraryEventProducer {
         Integer key = libraryEvent.getLibraryEventId();
         String value = objectMapper.writeValueAsString(libraryEvent);
 
-        ProducerRecord<Integer, String> producerRecord =  buildProducerRecord(key, value, topic);
+        ProducerRecord<Integer, String> producerRecord =  buildProducerRecordWithHeaders(key, value, topic);
         ListenableFuture<SendResult<Integer, String>> listenableFuture =  kafkaTemplate.send(producerRecord);
         listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
             @Override
@@ -67,6 +70,11 @@ public class LibraryEventProducer {
 
     private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value, String topic) {
         return new ProducerRecord<>(topic, null, key, value, null);
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecordWithHeaders(Integer key, String value, String topic) {
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
     }
 
     //synchronicznie
