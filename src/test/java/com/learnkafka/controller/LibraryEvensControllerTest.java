c6592d42f368dd5ef4fc7554d49b7c4a72aa3046
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-03-29 12:44:25 +0200

    10.44 Write JUnit Test for Kafka Producer - OnFailure()

--- src/test/java/com/learnkafka/controller/LibraryEvensControllerTest.java
+++ src/test/java/com/learnkafka/controller/LibraryEvensControllerTest.java
@@ -11,8 +11,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,7 +36,7 @@ public class LibraryEvensControllerTest {
         Book book = Book.builder().bookId(123).bookAuthor("Dziura").bookName("Kafka using Spring Boot").build();
         LibraryEvent libraryEvent = LibraryEvent.builder().libraryEventId(null).book(book).build();
         String json = objectMapper.writeValueAsString(libraryEvent);
-        doNothing().when(libraryEventProducer).sendLibraryEvent_Send(isA(LibraryEvent.class));
+        when(libraryEventProducer.sendLibraryEvent_Send(isA(LibraryEvent.class))).thenReturn(null);
 
         //expect
         mockMvc.perform(post("/v1/libraryevent").content(json).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
@@ -56,7 +57,7 @@ public class LibraryEvensControllerTest {
                 .build();
 
         String json = objectMapper.writeValueAsString(libraryEvent);
-        doNothing().when(libraryEventProducer).sendLibraryEvent_Send(isA(LibraryEvent.class));
+        when(libraryEventProducer.sendLibraryEvent_Send(isA(LibraryEvent.class))).thenReturn(null);
 
         //expect/
         String expectedErrorMessage = "book.bookAuthor - must not be blank, book.bookId - must not be null";
